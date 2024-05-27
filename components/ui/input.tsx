0a584b956da3ebba515e9edf605b71787aba34af
kangeunhwa <dmdgi37@naver.com>
2024-05-27 22:50:21 +0900

    server-build-test:빌드 오류 수정2

--- components/ui/input.tsx
+++ components/ui/input.tsx
@@ -3,7 +3,9 @@ import * as React from "react";
 import { cn } from "@/lib/utils";
 
 export interface InputProps
-  extends React.InputHTMLAttributes<HTMLInputElement> {}
+  extends React.InputHTMLAttributes<HTMLInputElement> {
+  icon?: string;
+}
 
 const Input = React.forwardRef<HTMLInputElement, InputProps>(
   ({ icon, className, type, ...props }, ref) => {
--- src/component/main/category.tsx
+++ src/component/main/category.tsx
@@ -1,16 +1,47 @@
+import { CATEGORY } from "@/src/utils/constants";
 import * as React from "react";
 
 interface ICategoryProps {}
 
 const Category: React.FunctionComponent<ICategoryProps> = (props) => {
-  const [activeCategory, setActiveCategory] = React.useState();
+  const [categoryActive, setCategoryActive] = React.useState("");
+  const onClickCategory = (e) => {
+    const name = e.target.attributes["data-name"].value;
+    categoryActive === name ? setCategoryActive("") : setCategoryActive(name);
+  };
 
   return (
-    <div>
-      <ul className="flex flex-col w-full h-full gap-2 list-none">
-        <li key="all" data-name="all" className=""></li>
-      </ul>
-    </div>
+    <ul className="flex flex-col w-full h-full gap-2 list-none">
+      <li
+        key="all"
+        data-name="all"
+        className={`p-4 px-5 ${
+          categoryActive === "all" ? "bg-gray-100 rounded-full" : ""
+        }`}
+        onClick={(e) => onClickCategory(e)}>
+        전체
+      </li>
+      {CATEGORY.map((name) => (
+        <li
+          key={name.engname}
+          data-name={name.engname}
+          className={`p-4 px-5 ${
+            categoryActive === name.engname ? "bg-gray-100 rounded-full" : ""
+          }`}
+          onClick={(e) => onClickCategory(e)}>
+          {name.korname}
+        </li>
+      ))}
+      <li
+        key="barter"
+        data-name="barter"
+        className={`p-4 px-5 ${
+          categoryActive === "barter" ? "bg-gray-100 rounded-full" : ""
+        }`}
+        onClick={(e) => onClickCategory(e)}>
+        교환하기
+      </li>
+    </ul>
   );
 };
 
