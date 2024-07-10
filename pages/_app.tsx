800591ef862420568e393c24660e09b055f8c31f
kangeunhwa <dmdgi37@naver.com>
2024-07-10 10:16:48 +0900

    build test 4

--- pages/_app.tsx
+++ pages/_app.tsx
@@ -29,7 +29,7 @@ export default function App({ Component, pageProps }: AppProps) {
   }, [router.pathname]);
   return (
     <>
-      {isAuthenticated ? <AuthHeader /> : <></>}
+      {isAuthenticated ? <AuthHeader /> : <UnAuthHeader />}
       <Component {...pageProps} />
     </>
   );
